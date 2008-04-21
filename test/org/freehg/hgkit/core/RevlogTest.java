d3dcc77895617681cd42bf86cdff3708694f6e35
stefan chyssler <stefan.chyssler@theinternet.com>
2008-04-21 08:36:22 +0200

    forgot to merge one file

--- test/org/freehg/hgkit/core/RevlogTest.java
+++ test/org/freehg/hgkit/core/RevlogTest.java
@@ -8,80 +8,61 @@ import org.junit.Test;
 
 public class RevlogTest {
 
-	@Test
-	public void testGetLatestRevision() {
-<<<<<<< local
-		
-		File index = new File(".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
-		index = new File(".hg/store/data/src/org/freehg/hgkit/core/_m_diff.java.i");
-		index = new File("../com.vectrace.MercurialEclipse/.hg/store/data/plugin.xml.i");
-		index = new File("../com.vectrace.MercurialEclipse/.hg/store/00changelog.i");
-		index = new File(".hg/store/00changelog.i");
-		
-=======
+    @Test
+    public void testGetLatestRevision() {
 
-		File index = new File(
-				".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
+        File index = new File(
+                ".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
+        index = new File(
+                ".hg/store/data/src/org/freehg/hgkit/core/_m_diff.java.i");
+        index = new File(
+                "../com.vectrace.MercurialEclipse/.hg/store/data/plugin.xml.i");
+        index = new File(
+                "../com.vectrace.MercurialEclipse/.hg/store/00changelog.i");
+        index = new File(".hg/store/00changelog.i");
 
->>>>>>> other
-		Revlog subject = new Revlog(index, index);
-		int numRev = subject.count();
-		log("Test file has : " + numRev + " revisions");
-		RevlogEntry tip = subject.tip();
+        Revlog subject = new Revlog(index, index);
+        int numRev = subject.count();
+        log("Test file has : " + numRev + " revisions");
+        RevlogEntry tip = subject.tip();
 
-		NodeId last = null;
-<<<<<<< local
-		log(subject);
-		
-		log(last);
-=======
-		System.out.println(subject);
+        log(subject);
 
-		System.out.println(last);
+        String revision = subject.revision(tip.getId());
+        log(" ################################ ");
+        log(revision);
 
->>>>>>> other
-		String revision = subject.revision(tip.getId());
-		log(" ################################ ");
-		log(revision);
-
-		for(NodeId rev : subject.getRevisions()) {
-		    subject.revision(rev);
-		}
-	}
-<<<<<<< local
-	
-	private void log(Object revision) {
-	    if(revision != null) {
-	        System.out.println(revision.toString());
-	    } else {
-	        System.out.println("null");
-	    }
+        for (NodeId rev : subject.getRevisions()) {
+            subject.revision(rev);
+        }
     }
-=======
->>>>>>> other
 
-<<<<<<< local
-    @Ignore
-=======
->>>>>>> other
-	@Test
-	public void testGetAllRevision() throws Exception {
-		// if( true ) return;
-		File index = new File(
-				".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
+    @Test
+    public void testGetAllRevision() throws Exception {
+        // if( true ) return;
+        File index = new File(
+                ".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
 
-		Revlog subject = new Revlog(index, index);
-		int count = 0;
+        Revlog subject = new Revlog(index, index);
+        int count = 0;
 
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < 1000; i++) {
-			for (NodeId nodeId : subject.getRevisions()) {
-				String revision = subject.revision(nodeId);
-			}
-		}
-		long end = System.currentTimeMillis();
-		
-		System.out.println("Took " + (end - start) + " ms");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            for (NodeId nodeId : subject.getRevisions()) {
+                String revision = subject.revision(nodeId);
+            }
+        }
+        long end = System.currentTimeMillis();
 
-	}
+        System.out.println("Took " + (end - start) + " ms");
+
+    }
+
+    private void log(Object revision) {
+        if (revision != null) {
+            System.out.println(revision.toString());
+        } else {
+            System.out.println("null");
+        }
+    }
 }
