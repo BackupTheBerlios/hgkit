2d86aced74b60944ffef11b3f1655600709ba751
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2008-09-08 16:35:05 +0200

    Implement unfolding and add some more testcases.

--- src/org/freehg/hgkit/core/CaseFolding.java
+++ src/org/freehg/hgkit/core/CaseFolding.java
@@ -1,95 +1,128 @@
 package org.freehg.hgkit.core;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
-// FIXME Implement this casefolding
+
+/**
+ * Implements folding and unfolding of filenames by replacing:
+ * <ul>
+ * <li>uppercase letters with underscore and uppercase letters</li>
+ * <li>reserved windows filesystem letters and non-ascii letters with their tilde-prefixed hex-code</li>
+ * <li>leaving the rest as is.<li>
+ * </ul>
+ * @author mirko
+ */
 public final class CaseFolding {
+    
+    /**
+     * Reserved letters in the Windows filesystem.
+     */
     final static char[] WIN_RESERVED = "\\:*?\"<>|".toCharArray();
-    final static HashMap<String,String> CMAP;
-    final static HashMap<String,String> DMAP;
+
+    /**
+     * Folding map.
+     */
+    final static HashMap<String, String> FOLD_MAP;
+
+    /**
+     * Unfolding map.
+     */
+    final static HashMap<String, String> UNFOLD_MAP;
+    
     static {
-        CMAP = new HashMap<String, String>();
-        DMAP = new HashMap<String, String>();
-        createLookupMap();        
+        FOLD_MAP = new HashMap<String, String>();
+        UNFOLD_MAP = new HashMap<String, String>();
+        createLookupMap();
         createInverseMap();
     }
+
     /**
-     * 
+     * Creates the map for folding.
      */
     private static void createLookupMap() {
         for (int i = 0; i < 127; i++) {
-            final Character c = (char)i;
-            CMAP.put(Character.toString(c), Character.toString(c));
+            final Character c = (char) i;
+            FOLD_MAP.put(Character.toString(c), Character.toString(c));
         }
         for (int i = 126; i < 256; i++) {
-            replaceSpecialLetters((char)i);
+            replaceSpecialLetters((char) i);
         }
-        for (char c:WIN_RESERVED) {
+        for (char c : WIN_RESERVED) {
             replaceSpecialLetters(c);
         }
-        replaceSpecialLetters((char)32);
+        replaceSpecialLetters((char) 32);
         replaceUpperCaseLetters();
     }
 
     /**
-     * 
+     * Replaces uppercase letters and underscore by underscore + lowercase
+     * letter in {@link CaseFolding#FOLD_MAP}.
      */
     private static void replaceUpperCaseLetters() {
-        for (char c = 'A'; c<= 'Z'; c++) {
-            CMAP.put(Character.toString(c), "_" + Character.toString(Character.toLowerCase(c)));
+        for (char c = 'A'; c <= 'Z'; c++) {
+            FOLD_MAP.put(Character.toString(c), "_" + Character.toLowerCase(c));
         }
-        CMAP.put("_", "__");
+        FOLD_MAP.put("_", "__");
     }
-    
-    /**
-     * Create the reverse map for unfolding. 
-     */
-    private static void createInverseMap() {
-        Set<String> keySet = CMAP.keySet();
-        for (String key : keySet) {
-            DMAP.put(CMAP.get(key), key);            
-        }
-    }
-    
-    /**
-     * Replace special characters with their Hex-Code. 
-     * @param c
-     */
-    private static void replaceSpecialLetters(final Character c) {
-        final String replacement = String.format("~%02x", (int)c);
-        CMAP.put(Character.toString(c), replacement);
-    }
-    
-	public static String fold(String name) {
-	    StringBuilder folded = new StringBuilder();
-        char[] charArray = name.toCharArray();
-        for (char c : charArray) {            
-            folded.append(CMAP.get(Character.toString(c)));
-        }
-        return folded.toString();
-	}
 
     /**
-     * @param map
-     * @param name
-     * @return
+     * Creates the reverse map {@link CaseFolding#UNFOLD_MAP} for unfolding.
      */
-    private static String replace(final HashMap<String, String> map, String name) {
-        StringBuilder folded = new StringBuilder();
-	    char[] charArray = name.toCharArray();
-	    for (char c : charArray) {            
-            folded.append(map.get(Character.toString(c)));
+    private static void createInverseMap() {
+        Set<Entry<String, String>> entrySet = FOLD_MAP.entrySet();
+        for (Entry<String, String> entry : entrySet) {
+            UNFOLD_MAP.put(entry.getValue(), entry.getKey());
         }
-	    return folded.toString();
     }
-	
-	public static String unfold(String foldedName) {
-		return replace(DMAP, foldedName);
-	}
-	
-	public static void main(String[] args) {
-        System.err.println(CMAP);
+
+    /**
+     * Replaces or sets special character with it's tilde prefixed hex-code in
+     * {@link CaseFolding#FOLD_MAP}.
+     * 
+     * @param c
+     *            character to replace.
+     */
+    private static void replaceSpecialLetters(final Character c) {
+        final String replacement = String.format("~%02x", (int) c);
+        FOLD_MAP.put(Character.toString(c), replacement);
+    }
+
+    /**
+     * Folds a pathname as Mercurial does in util.encodefilename.
+     * 
+     * @param name
+     * @return the folded name.
+     */
+    public static String fold(String name) {
+        StringBuilder folded = new StringBuilder();
+        char[] charArray = name.toCharArray();
+        for (char c : charArray) {
+            folded.append(FOLD_MAP.get(Character.toString(c)));
+        }
+        return folded.toString();
+    }
+
+    /**
+     * Unfolds a pathname as Mercurial does in util.decodefilename.
+     * 
+     * @param foldedName
+     * @return the unfolded name.
+     */
+    public static String unfold(String foldedName) {
+        final StringBuilder unfolded = new StringBuilder();
+        for (int i = 0; i < foldedName.length();) {
+            final char[] keyCharacters;
+            if (foldedName.charAt(i) == '~') {
+                keyCharacters = new char[] { foldedName.charAt(i++), foldedName.charAt(i++), foldedName.charAt(i++) };
+            } else if (foldedName.charAt(i) == '_') {
+                keyCharacters = new char[] { foldedName.charAt(i++), foldedName.charAt(i++) };
+            } else {
+                keyCharacters = new char[] { foldedName.charAt(i++) };
+            }
+            unfolded.append(UNFOLD_MAP.get(new String(keyCharacters)));
+        }
+        return unfolded.toString();
     }
 }
--- test/org/freehg/hgkit/core/CaseFoldingTest.java
+++ test/org/freehg/hgkit/core/CaseFoldingTest.java
@@ -1,27 +1,52 @@
 package org.freehg.hgkit.core;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 
+@RunWith(Parameterized.class)
 public class CaseFoldingTest {
 
-    private static final String TEST_STRING = "the_path is Here/HgStatus.java";
+    private final String unfolded;
+    private final String folded;
 
+    public CaseFoldingTest(final String unfolded, final String folded) {
+        this.unfolded = unfolded;
+        this.folded = folded;
+    }
+    
+    @Parameters
+    public static Collection<String[]> data() {
+        return Arrays.asList(        
+                new String[] {":\\", "~3a~5c"},
+                new String[] {"the_path is Here/HgStatus.java", "the__path~20is~20_here/_hg_status.java"},
+                new String[] {"C:\\WindowS\\foo.*", "_c~3a~5c_window_s~5cfoo.~2a"}
+                );
+    }
+    
+    /**
+     * Testmethod for {@link CaseFolding#fold(String)}.
+     */
     @Test
     public void testFold() {
-        String result = CaseFolding.fold(TEST_STRING);
-        System.err.println(result);
-        assertFalse(result.equals(TEST_STRING));
+        String result = CaseFolding.fold(unfolded);        
+        assertEquals(folded, result);        
     }
 
+    /**
+     * Tests wether {@link CaseFolding#fold(String)} and {@link CaseFolding#unfold(String)} are symmetric.
+     */
     @Test
-    public void testUnfold() {
-        String result = CaseFolding.unfold(CaseFolding.fold(TEST_STRING));
-        assertEquals(TEST_STRING, result);
-        
+    public void testSymmetry() {
+        String result = CaseFolding.unfold(CaseFolding.fold(unfolded));
+        assertEquals(unfolded, result);        
     }
 
 }
