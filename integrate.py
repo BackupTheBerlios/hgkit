c8d04f109ec34d554080adf71e50504c5d92c0c8
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-09-10 22:53:29 +0200

    Deploy as well

--- integrate.py
+++ integrate.py
@@ -15,7 +15,7 @@ def main(sleep=60):
         remoteid = getRepoId("http://hg.berlios.de/repos/hgkit")
         logging.info("localid=%s, remoteid=%s" % (localid, remoteid))
         if localid != remoteid:
-            os.system("mvn scm:update clean site")
+            os.system("mvn scm:update clean site site:deploy")
         logging.info("Sleeping for %d seconds" % sleep)
         time.sleep(sleep)
 
