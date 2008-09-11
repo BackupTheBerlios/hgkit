602a9b421d42809c4dd22c23881e4506a43c7559
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-09-11 21:13:18 +0200

    Revlog works for small files, not for big ones

--- src/test/java/org/freehg/hgkit/core/RevlogTest.java
+++ src/test/java/org/freehg/hgkit/core/RevlogTest.java
@@ -4,27 +4,48 @@ import static org.junit.Assert.assertFalse;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class RevlogTest {
+
+    private File index;
+
+    /**
+     * 
+     */
+    public RevlogTest(String indexName) {
+        index = new File("src/test/resources/", indexName);
+    }
+
+    @Parameters
+    public static Collection<String[]> data() {
+        return Arrays.asList(new String[] { "test1.txt.i" }, new String[] { "test2.txt.i" },
+                new String[] { "test3.txt.i" }, new String[] { "test4.txt.i" }, new String[] { "test5.txt.i" },
+                new String[] { "bigger.txt.i" });
+    }
 
     @Test
     public void testGetLatestRevision() {
-
-        File index = new File(".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
-
         Revlog subject = new Revlog(index);
         int numRev = subject.count();
-        log("Test file has : " + numRev + " revisions");
-        RevlogEntry tip = subject.tip();
+        System.err.println("Test file " + index + " has : " + numRev + " revisions");
         String prevRevision = null;
         NodeId prevNodeId = null;
-        for (NodeId nodeId : subject.getRevisions()) {            
+        for (NodeId nodeId : subject.getRevisions()) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             subject.revision(nodeId, out);
             final String currentRevision = out.toString();
+            System.err.println(nodeId + ":" + currentRevision);
             assertFalse("Revisions " + nodeId + " and " + prevNodeId + " of " + index + " must not be equal!",
                     currentRevision.equals(prevRevision));
             prevRevision = currentRevision;
@@ -33,6 +54,7 @@ public class RevlogTest {
         subject.close();
     }
 
+    @Ignore
     @Test
     public void testGetAllRevision() throws Exception {
         // if( true ) return;
