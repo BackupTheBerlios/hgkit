d38f70b04a20cfa5888012b9facc9cf62145c8ba
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-09-12 17:20:13 +0200

    Performance Tests

--- src/test/java/org/freehg/hgkit/core/RevlogTest.java
+++ src/test/java/org/freehg/hgkit/core/RevlogTest.java
@@ -5,13 +5,11 @@ import static org.junit.Assert.assertFalse;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Set;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.theories.DataPoint;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -38,22 +36,23 @@ public class RevlogTest {
         for (String testFile : testFiles) {
             result.add(new String[] { "src/test/resources/" + testFile });
         }
-        result.add(new String[]{".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i"});
+        result.add(new String[] { ".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i" });
         return result;
     }
 
     @Test
     public void testGetLatestRevision() {
         Revlog subject = new Revlog(index);
-        int numRev = subject.count();
-        System.err.println("Test file " + index + " has : " + numRev + " revisions");
+        // int numRev = subject.count();
+        // System.err.println("Test file " + index + " has : " + numRev +
+        // " revisions");
         String prevRevision = null;
         NodeId prevNodeId = null;
         for (NodeId nodeId : subject.getRevisions()) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             subject.revision(nodeId, out);
             final String currentRevision = out.toString();
-//            System.err.println(nodeId + ":" + currentRevision.length());
+            // System.err.println(nodeId + ":" + currentRevision.length());
             assertFalse("Revisions " + nodeId + " and " + prevNodeId + " of " + index + " must not be equal!",
                     currentRevision.equals(prevRevision));
             prevRevision = currentRevision;
@@ -62,9 +61,9 @@ public class RevlogTest {
         subject.close();
     }
 
-    @Ignore
+    @Ignore(value="performancetest")
     @Test
-    public void testGetAllRevision() throws Exception {
+    public void testGetAllRevision() {
         // if( true ) return;
         File index = new File(".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
 
@@ -94,6 +93,43 @@ public class RevlogTest {
             System.out.println(revision.toString());
         } else {
             System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+        RevlogTest revlogTest = new RevlogTest(".hg/store/data/src/org/freehg/hgkit/_hg_status_client.java.i");
+        testUncached(revlogTest);
+        testCached(revlogTest);
+    }
+
+    /**
+     * This test is not really cached, as caching is disabled in {@link Revlog} right now.
+     * However after reenabling it should run much faster than {@link RevlogTest#testUncached(RevlogTest)}.
+     * @param revlogTest
+     */
+    private static void testCached(RevlogTest revlogTest) {
+        long start = System.nanoTime();
+        revlogTest.testGetAllRevision();
+        long end = System.nanoTime();
+        System.err.println("testGetAllRevision took " + (end - start) / 1000000 + " ms. (Cached)");
+    }
+
+    /**
+     * @param revlogTest
+     */
+    private static void testUncached(RevlogTest revlogTest) {
+        {
+            long start = System.nanoTime();
+            final int iterations = 1000;
+            for (int i = 0; i < iterations; i++) {
+                if (i % 100 == 0) {
+                    System.err.println(i);
+                }
+                revlogTest.testGetLatestRevision();
+            }
+            long end = System.nanoTime();
+            System.err.println(iterations + " iterations of testGetLatestRevision took " + (end - start) / 1000000
+                    + " ms.");
         }
     }
 }
