8fd89ae6664eb22b627818a536ac8f284390d132
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-09-24 20:51:02 +0200

    prepare RemoveMetaOutputStreamTest step 3

--- src/test/java/org/freehg/hgkit/util/RemoveMetaOutputStreamTest.java
+++ src/test/java/org/freehg/hgkit/util/RemoveMetaOutputStreamTest.java
@@ -9,10 +9,19 @@
 package org.freehg.hgkit.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
+import org.freehg.hgkit.Tutil;
+import org.freehg.hgkit.core.NodeId;
+import org.freehg.hgkit.core.Repository;
+import org.freehg.hgkit.core.Revlog;
+import org.freehg.hgkit.core.RevlogEntry;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -24,16 +33,27 @@ public class RemoveMetaOutputStreamTest {
     private static final String DATA = "##\n# User Database\n# ";
 
     private static final String META_DATA = "\ncopyrev: f19cc7cb916200d98e4ef68d7715604da8a4a09f\ncopy: src/test/java/passwd\n\n";
+    private static File repoDir;
+
+    @BeforeClass
+    public static void createCopy() {
+        repoDir = Tutil.createRepoCopy();
+    }
+
+    @AfterClass
+    public static void deleteCopy() {
+        assertTrue("Could not delete copy in " + repoDir, FileHelper.deleteDirectory(repoDir));
+    }
 
     @Test
-    public void testWithMetaData() throws IOException {
-        byte[] input = (META_DATA + DATA).getBytes();
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        RemoveMetaOutputStream removeMetaOutputStream = new RemoveMetaOutputStream(out);
-        for (byte b : input) {
-            removeMetaOutputStream.write(b);
-        }        
-        assertEquals("", new String(out.toByteArray()));
+    public void testWithMetaData() {
+        Repository repository = new Repository(repoDir);
+        File absolute = repository.makeAbsolute("src/test/resources/moved-file");
+        Revlog revlog = repository.getRevlog(absolute);
+        NodeId nodeId = revlog.tip().getId();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        revlog.revision(nodeId, out);
+        assertEquals("this file is moved and has Metadata", out.toString());
     }
     
     @Test
