9daeaa12b0cd8140b313745a5cdc1725ab3b468a
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-11-02 23:55:36 +0100

    Almost useless little test to increase coverage

---
+++ src/test/java/org/freehg/hgkit/FileStatusTest.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2008 Mirko Friedenhagen
+ * 
+ * This software may be used and distributed according to the terms of
+ * the GNU General Public License or under the Eclipse Public Licence (EPL)
+ *
+ */
+
+package org.freehg.hgkit;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+/**
+ * @author mirko
+ */
+public class FileStatusTest {
+
+    /**
+     * Test method for {@link org.freehg.hgkit.FileStatus#toString()}.
+     */
+    @Test
+    public final void testToString() {
+        FileStatus fileStatus = new FileStatus(new File("/"), FileStatus.Status.ADDED);
+        assertEquals("A /", fileStatus.toString());
+    }
+
+}
