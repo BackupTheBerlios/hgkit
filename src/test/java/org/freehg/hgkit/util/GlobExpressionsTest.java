cbc54e0e34756c97f87845d3ac9bb92d35675098
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-11-03 21:19:19 +0100

    Datadriven test

--- src/test/java/org/freehg/hgkit/util/GlobExpressionsTest.java
+++ src/test/java/org/freehg/hgkit/util/GlobExpressionsTest.java
@@ -1,14 +1,39 @@
 package org.freehg.hgkit.util;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class GlobExpressionsTest {
+
+    private final String expected;
+
+    private final String glob;
+
+    public GlobExpressionsTest(final String expected, final String glob) {
+        this.expected = expected;
+        this.glob = glob;
+
+    }
+
+    @Parameters
+    public static Collection<String[]> data() {
+        ArrayList<String[]> data = new ArrayList<String[]>();
+        data.add(new String[] { ".*\\..*", "*.*" });
+        data.add(new String[] { ".*.*\\/.*.*", "**/**" });
+        data.add(new String[] { "He.lig", "He?lig" });
+        return data;
+    }
 
     @Test
     public void testGlobCompiler() {
-        System.out.println(GlobExpressions.toRegex("*.*").pattern());
-        System.out.println(GlobExpressions.toRegex("**/**").pattern());
-        System.out.println(GlobExpressions.toRegex("He?lig").pattern());
+        assertEquals(expected, GlobExpressions.toRegex(glob).pattern());
     }
-    
 }
