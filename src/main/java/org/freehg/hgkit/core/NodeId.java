463be236dd9a39ff51a30bcf9f15bbdc2f9aa299
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-11-03 22:07:55 +0100

    Document NodeId

--- src/main/java/org/freehg/hgkit/core/NodeId.java
+++ src/main/java/org/freehg/hgkit/core/NodeId.java
@@ -4,16 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+/**
+ * Describe a nodeId.
+ */
 public final class NodeId {
 
-    /**
-     * 
-     */
     private static final int HASHCODE_PRIME = 31;
 
-    /**
-     * 
-     */
     private static final int NODE_ID_LENGTH = 20;
 
     public static final int SHA_SIZE = 20;
@@ -22,41 +19,59 @@ public final class NodeId {
 
     private static final int SIZE = 32;
 
-    private byte[] nodeid;
+    private final byte[] nodeid;
 
     private int hash = -1;
 
+    /**
+     * Constructs a NodeId from data.
+     * 
+     * @param data
+     *            data
+     */
     private NodeId(byte[] data) {
         // if using 20 bytes node id, make it a 32 byte with trailing zeros
         if (data.length < SIZE) {
-            this.nodeid = new byte[32];
-            System.arraycopy(data, 0, this.nodeid, 0, data.length);
+            nodeid = new byte[SIZE];
+            System.arraycopy(data, 0, nodeid, 0, data.length);
         } else {
             nodeid = data;
         }
     }
 
+    /** {@inheritDoc} */
     @Override
     public int hashCode() {
-        if (this.hash != -1) {
-            return this.hash;
+        if (hash != -1) {
+            return hash;
         }
         // will replace the constant in the calculation,
-                              // but keep this declaration.
-        int result = 1;
-        result = HASHCODE_PRIME * result + Arrays.hashCode(nodeid);
-        this.hash = result;
-        return result;
+        // but keep this declaration.
+        hash = HASHCODE_PRIME + Arrays.hashCode(nodeid);
+        return hash;
     }
 
+    /**
+     * Factory method for NodeIds. data.length must be either
+     * {@link NodeId#SHA_SIZE} or {@link NodeId#SIZE}.
+     * 
+     * @param data
+     *            nodeId data
+     * @return a new NodeId
+     */
     public static NodeId valueOf(byte[] data) {
-        if (!(data.length == SIZE || data.length == SHA_SIZE)) {
+        switch (data.length) {
+        case SIZE:
+            return new NodeId(data);
+        case SHA_SIZE:
+            return new NodeId(data);
+        default:
             throw new IllegalArgumentException("NodeId byte size must be either " + SHA_SIZE + " or " + SIZE
                     + " but was " + data.length);
         }
-        return new NodeId(data);
     }
 
+    /** {@inheritDoc} */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -75,20 +90,38 @@ public final class NodeId {
         return true;
     }
 
+    /**
+     * Returns the short string representation of the nodeId.
+     * 
+     * @return nodeId
+     */
     public String asShort() {
         return toString(SHORT_SIZE);
     }
 
+    /**
+     * Returns the complete SHA string representation of the nodeId.
+     * 
+     * @return nodeId
+     */
     public String asFull() {
         return toString(SHA_SIZE);
 
     }
 
+    /** {@inheritDoc} */
     @Override
     public String toString() {
         return asShort();
     }
 
+    /**
+     * Returns a part of the nodeId as Hex-String.
+     * 
+     * @param length
+     *            how long should the nodeId be
+     * @return nodeId
+     */
     public String toString(int length) {
         StringBuilder tos = new StringBuilder();
         for (int i = 0; i < length; i++) {
@@ -104,6 +137,13 @@ public final class NodeId {
         return tos.toString();
     }
 
+    /**
+     * Returns the byte representation of a Hex-String.
+     * 
+     * @param hexStr
+     *            to convert.
+     * @return byte array
+     */
     public static byte[] toBinArray(String hexStr) {
         byte bArray[] = new byte[hexStr.length() / 2];
         for (int i = 0; i < (hexStr.length() / 2); i++) {
@@ -113,7 +153,7 @@ public final class NodeId {
             // y
             // )
             byte secondNibble = Byte.parseByte(hexStr.substring(2 * i + 1, 2 * i + 2), 16);
-            int finalByte = (secondNibble) | (firstNibble << 4); //bit-operations
+            int finalByte = (secondNibble) | (firstNibble << 4); // bit-operations
             // only with
             // numbers, not
             // bytes.
@@ -122,11 +162,16 @@ public final class NodeId {
         return bArray;
     }
 
+    /**
+     * Creates a new NodeId from the given Hex-String
+     * 
+     * @param nodeId
+     *            as Hex-String
+     * @return nodeId
+     */
     public static NodeId parse(String nodeId) {
-
-        byte[] bytes = toBinArray(nodeId);
-        NodeId result = valueOf(bytes);
-        String asFull = result.asFull();
+        final NodeId result = valueOf(toBinArray(nodeId));
+        final String asFull = result.asFull();
         if (!nodeId.equals(asFull)) {
             System.err.println(asFull + " != " + nodeId);
             // FIXME Figure out when and why this happens!
