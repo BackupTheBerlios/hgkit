13d535978f46163b3b4547f301c1729d5034b895
Mirko Friedenhagen <mfriedenhagen@users.berlios.de>
2008-12-08 23:53:41 +0100

    Document Manifest a bit

--- src/main/java/org/freehg/hgkit/core/Manifest.java
+++ src/main/java/org/freehg/hgkit/core/Manifest.java
@@ -1,43 +1,82 @@
 package org.freehg.hgkit.core;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
+import org.freehg.hgkit.HgInternalError;
+
+/**
+ * The manifest is the file that describes the contents of the repository at a
+ * particular changeset ID.
+ * 
+ * @see <a href="http://www.selenic.com/mercurial/wiki/index.cgi/Manifest">Manifest</a>
+ */
 public class Manifest extends Revlog {
 
+    /**
+     * A manifest line consists of the length of the filename followed by
+     * filename and nodeId.
+     */
+    private static class NodeFilemap {
+        
+        private final Map<String, NodeId> nodefilemap = new HashMap<String, NodeId>();
+        
+        /**
+         * @return the nodefilemap
+         */
+        public Map<String, NodeId> getNodefilemap() {
+            return nodefilemap;
+        }
+        
+        /**
+         * See {@link Manifest#get(org.freehg.hgkit.core.ChangeLog.ChangeSet)}.
+         */
+        private static NodeFilemap valueOf(Collection<String> lines) {
+            final NodeFilemap instance = new NodeFilemap();
+            for (final String line : lines) {
+                final int nameLength = line.indexOf(0);
+                final String name = line.substring(0, nameLength);
+                final String nodeStr = line.substring(nameLength + 1, nameLength + 1 + NodeId.SHA_SIZE * 2);
+                final NodeId node = NodeId.valueOf(nodeStr);
+                instance.nodefilemap.put(name, node);
+            }
+            return instance;
+            
+        }
+    }
+    /**
+     * Constructor for the manifestfile.
+     * 
+     * @param index
+     *            normally <code>.hg/store/00manifest.[id]{1}</code>
+     */
     public Manifest(File index) {
         super(index);
     }
 
+    /**
+     * Returns a map of paths and their corresponding nodeIds.
+     * 
+     * @param changelog
+     *            for which changeset do we want the manifest entries.
+     * @return manifest entries.
+     */
     public Map<String, NodeId> get(ChangeLog.ChangeSet changelog) {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
         super.revision(changelog.getManifestId(), out).close();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(out.toByteArray())));
-        return parse(reader);
-    }
-
-    private Map<String, NodeId> parse(BufferedReader reader) {
-
-        Map<String, NodeId> nodefilemap = new HashMap<String, NodeId>();
         try {
-            String line = null;
-            while (null != (line = reader.readLine())) {
-                int cutAt = line.indexOf(0);
-                String name = line.substring(0, cutAt);
-                String nodeStr = line.substring(cutAt + 1, cutAt + 1 + NodeId.SHA_SIZE * 2);
-                NodeId node = NodeId.valueOf(nodeStr);
-                nodefilemap.put(name, node);
-
-            }
+            @SuppressWarnings("unchecked")
+            final List<String> lines = IOUtils.readLines(new ByteArrayInputStream(out.toByteArray()));            
+            return NodeFilemap.valueOf(lines).getNodefilemap();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new HgInternalError(changelog.toString(), e);
         }
-        return nodefilemap;
     }
 }
